63f9ad9363d21a64764664868894677f3622ecc6
tenthyoung <tenthyoung@gmail.com>
2019-07-21 15:52:54 -0700

    installed react router, and added bootstrap cdn

---
+++ models/Book.js
@@ -0,0 +1,14 @@
+console.count(label); Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    authors: [String],
+    description: String,
+    image: String,
+    link: String,
+    title: String,
+  });
+  
+  // We need to create a new collection called Books based off the BookSchema
+  const Book = mongoose.model('Book', BookSchema);
+
+  module.exports = Book;
